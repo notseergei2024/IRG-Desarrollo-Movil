48f82deb09416c98030695adc9a89a1ba6e2fe82
Victor Manzano <vmanzanobartual@gmail.com>
2026-05-04 15:42:15 +0200

    Delete sergio-agenda directory

--- sergio-agenda/sergio-agenda/data/agenda.js
+++
@@ -1,7 +0,0 @@
-// data/agenda.js
-// DEPRECADO. Los datos iniciales están ahora en data/seed.js y la
-// persistencia/CRUD se hace desde services/citasService.js y
-// services/clientesService.js. Este archivo se mantiene solo para
-// no romper imports antiguos: re-exporta lo necesario.
-
-export { EMPLEADOS, CITAS_SEED as CITAS } from './seed';
